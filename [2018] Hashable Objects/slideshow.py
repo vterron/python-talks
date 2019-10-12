a14d073377bdd9ae66567ef7827bd498c4e3623a
Víctor Terrón <quintanar@gmail.com>
2019-10-12 10:10:17 +0200

    "Alternative" slides is a more accurate term than "extended"

--- [2018] Hashable Objects/slideshow.py
+++ [2018] Hashable Objects/slideshow.py
@@ -12,9 +12,9 @@ NBCONVERT_OPTS=--SlidesExporter.reveal_transition=none \
    	           --reveal-prefix=https://cdn.jsdelivr.net/reveal.js/3.0
 jupyter nbconvert ${NOTEBOOK_FILE} --to slides ${NBCONVERT_OPTS}
 
-... plus the ability of generating an extended version of the slides via
-the --extended flag. For more information, refer to the docstring of the
-ExtendedSlidesExporter class in this module.
+... plus the ability of generating an alternative version of the slides via
+the --alternative flag. For more information, refer to the docstring of the
+AlternativeSlidesExporter class in this module.
 
 """
 
@@ -28,17 +28,20 @@ import nbformat
 import traitlets
 import traitlets.config
 
-class ExtendedSlidesExporter(nbconvert.exporters.SlidesExporter):
-    """A nbconvert exporter to generate an 'extended' version of slides.
+
+class AlternativeSlidesExporter(nbconvert.exporters.SlidesExporter):
+    """A nbconvert exporter to generate an alternative version of slides.
 
     This nbconvert exporter subclasses SlidesExporter to add additional
     functionality: the ability to use the same notebook to generate two
     different versions of the presentation: (a) the regular one and (b)
-    the 'extended' (also known as 'Director's Cut') one.
+    the alternative one.
 
-    This is achieved via cell tags, which are expected to start with
-    "extended:" and follow with the value that overrides the slideshow's
-    "slide_type" when the extended version is generated. For example...
+    This is achieved via cell tags, which must start with "alternative:"
+    and follow with the value that overrides the slideshow's "slide_type"
+    when the alternative version is generated.
+
+    For example...
 
     [...]
     "metadata": {
@@ -46,45 +49,43 @@ class ExtendedSlidesExporter(nbconvert.exporters.SlidesExporter):
       "slide_type": "skip"
      },
      "tags": [
-      "extended:subslide"
+      "alternative:subslide"
      ]
     },
     [...]
 
     ... means that the slide is normally skipped ("slide_type": "skip"),
-    but rendered as a subslide (because of tag "extended:subslide") when
-    the extended version is generated.
+    but rendered as a subslide (because of the "alternative:subslide" tag)
+    when the alternative version is generated.
 
     You may also use this functionality to get the opposite effect: a
-    slide that is by default rendered but that, via the "extended:skip"
-    tag, is skipped when the extended version is generated.
+    slide that is by default rendered but that, via the "alternative:skip"
+    tag, is skipped when the alternative version is generated.
     """
 
-    extended_edition = traitlets.Bool(
-        help="Generate the director's cut (extended) version of the slides",
+    alternative_edition = traitlets.Bool(
+        help="Generate the alternative version of the slides",
         default_value=False).tag(config=True)
 
-
     def prepare(self, nb):
-        if not self.extended_edition:
+        if not self.alternative_edition:
             return nb
         nb = copy.deepcopy(nb)
         for index, cell in enumerate(nb.cells):
             self.prepare_cell(cell, index)
         return nb
 
-
     @staticmethod
     def prepare_cell(cell, index):
-        tags = [t for t in cell.metadata.get('tags', []) if t.startswith("extended:")]
+        tags = [t for t in cell.metadata.get('tags', []) if t.startswith("alternative:")]
 
         if not tags:
             return
         if len(tags) > 1:
-            raise ValueError("Multiple extended:* tags in cell (index={})".format(index))
+            raise ValueError("Multiple alternative:* tags in cell (index={})".format(index))
         if tags:
             _, slide_type = tags.pop().split(":")
-            print('Rendering slide (index={}) with type={}'.format(index, slide_type))
+            print('Rendering alternative slide (index={}) with type={}'.format(index, slide_type))
             cell.metadata.slideshow.slide_type = slide_type
 
     def from_notebook_node(self, nb, **kwargs):
@@ -97,19 +98,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("notebook")
     parser.add_argument(
-        "-e", "--extended",
+        "-a", "--alternative",
         action="store_true",
-        help="Generate the director's cut (extended) version of the slides")
+        help="Generate the alternative version of the slides")
     args = parser.parse_args()
 
     with open(args.notebook) as fd:
         contents = fd.read()
 
     c = traitlets.config.Config()
-    c.ExtendedSlidesExporter.reveal_transition = 'none'
-    c.ExtendedSlidesExporter.reveal_url_prefix = 'https://cdn.jsdelivr.net/reveal.js/3.0'
-    c.ExtendedSlidesExporter.extended_edition = args.extended
-    exporter = ExtendedSlidesExporter(config=c)
+    c.AlternativeSlidesExporter.reveal_transition = 'none'
+    c.AlternativeSlidesExporter.reveal_url_prefix = 'https://cdn.jsdelivr.net/reveal.js/3.0'
+    c.AlternativeSlidesExporter.alternative_edition = args.alternative
+    exporter = AlternativeSlidesExporter(config=c)
     notebook = nbformat.reads(contents, as_version=4)
     body, _ = exporter.from_notebook_node(notebook)
 
